e336d200da436d54689dccf6e8c3759bc7a5e0e1
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-02-26 12:50:59 -0700

    Set up componentHeader that includes Nav-bar

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Nav from './Nav';
+
+const Header = () => (
+  <div>
+    <div classname="bar">
+      <a href="">GlobeTrotter</a>
+      <Nav />
+    </div>
+    <div classname="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+)
+
+  
+export default Header;
