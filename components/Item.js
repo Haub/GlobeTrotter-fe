ec94a459f8a9e3e35bcda4384d6a29c3ad2c478f
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-03-08 16:34:19 -0700

    Add Link to Item component

--- components/Item.js
+++ components/Item.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
 
 
 export default class Item extends Component {
@@ -10,9 +13,18 @@ export default class Item extends Component {
 
   render() {
     return(
-      <div>
-
-      </div>
+      <ItemStyles>
+        <Title>
+          <Link
+            href={{
+              pathname: '/item',
+              query: { id: item.id },
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+      </ItemStyles>
     )
 
   }
