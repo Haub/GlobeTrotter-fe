98ce49f50921ef2c4c4080bf59ce8e8aa899481e
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-03-15 13:56:54 -0600

    Add form with inputs for createItem

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
-import Form from './styles/Form'l
+import Form from './styles/Form';
 import gql from 'graphql-tag';
 import formatMoney from '../lib/formatMoney';
 
@@ -19,8 +19,9 @@ class CreateItem extends Component {
 
   handleChange= (event) => {
     console.log(event);
-    const {name, value} = event.target;
-    this.setState({ [name]: value})
+    const {name, type, value} = event.target;
+     const val = type === 'number' ? parseFloat(value) : value;
+    this.setState({ [name]: val });
   }
 
   render() {
@@ -35,6 +36,30 @@ class CreateItem extends Component {
               placeholder="Title"
               required
               value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor='price'>
+            <input 
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor='description'>
+            <input 
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Description"
+              required
+              value={this.state.description}
               onChange={this.handleChange}
             />
           </label>
