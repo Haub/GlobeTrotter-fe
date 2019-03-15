e07c465cd6212b308ab36bfd917f31450bdfd8bc
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-03-15 11:33:10 -0600

    Add CreateItem component for items

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
+import Form from './styles/Form'l
 import gql from 'graphql-tag';
+import formatMoney from '../lib/formatMoney';
 
 
 class CreateItem extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title: '',
+      description: '',
+      image: '',
+      largeImage: '',
+      price: 0
+    }
+  }
+
+  handleChange= (event) => {
+    console.log(event);
+    const {name, value} = event.target;
+    this.setState({ [name]: value})
+  }
+
   render() {
     return(
-      <div>
+      <Form>
+        <fieldset>
+          <label htmlFor='title'>
+            <input 
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+        </fieldset>
         
-      </div>
+      </Form>
 
     )
   }
